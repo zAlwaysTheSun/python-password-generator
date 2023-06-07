b62c8da13591f65c1bff9a5d878d1797a86f8800
zaz <110661702+zazdev123@users.noreply.github.com>
2023-06-07 09:26:11 +0200

    Update python_password_generator.py

--- python_password_generator.py
+++ python_password_generator.py
@@ -1,94 +1,105 @@
 import tkinter as tk
-import tkinter.ttk as ttk
-import pyperclip
-from tkinter import filedialog
 from tkinter import messagebox
+from cryptography.fernet import Fernet
 import random
 import string
-import os
 
-def generate_password(length, include_digits=False, include_uppercase=False, include_special=False):
-    # Créer une liste de caractères possibles en fonction des options choisies
-    chars = string.ascii_lowercase
-    if include_digits:
-        chars += string.digits
-    if include_uppercase:
+# Génère un mot de passe aléatoire en fonction des options sélectionnées
+def generate_password():
+    length = int(length_entry.get())
+    use_lowercase = lowercase_var.get()
+    use_uppercase = uppercase_var.get()
+    use_digits = digits_var.get()
+    use_special_chars = special_chars_var.get()
+
+    chars = ""
+    if use_lowercase:
+        chars += string.ascii_lowercase
+    if use_uppercase:
         chars += string.ascii_uppercase
-    if include_special:
+    if use_digits:
+        chars += string.digits
+    if use_special_chars:
         chars += string.punctuation
 
-    # Générer un mot de passe aléatoire en utilisant la liste de caractères possibles
+    if not chars:
+        messagebox.showerror("Erreur", "Veuillez sélectionner au moins une option.")
+        return
+
     password = ''.join(random.choice(chars) for _ in range(length))
-    return password
+    password_entry.delete(0, tk.END)
+    password_entry.insert(tk.END, password)
 
-def export_password(password):
-    filename = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Fichier texte", "*.txt")])
-    if filename:
-        with open(filename, "w") as f:
-            f.write(password)
+# Enregistre le mot de passe dans un fichier TXT crypté
+def save_password():
+    password = password_entry.get()
+    name = name_entry.get()
 
-def generate_password_gui():
-    # Créer une fenêtre Tkinter
-    window = tk.Tk()
-    window.title("Générateur de mot de passe | Shoqapique#0001 on Discord")
-    # Ajouter des widgets pour les options de mot de passe
-    length_label = tk.Label(window, text="Nombre de caractères :")
-    length_label.grid(row=0, column=0, padx=5, pady=5)
-    length_entry = tk.Entry(window)
-    length_entry.grid(row=0, column=1, padx=5, pady=5)
-    digits_var = tk.BooleanVar()
-    digits_check = tk.Checkbutton(window, text="Chiffres", variable=digits_var)
-    digits_check.grid(row=1, column=0, padx=5, pady=5)
-    uppercase_var = tk.BooleanVar()
-    uppercase_check = tk.Checkbutton(window, text="Majuscules", variable=uppercase_var)
-    uppercase_check.grid(row=1, column=1, padx=5, pady=5)
-    special_var = tk.BooleanVar()
-    special_check = tk.Checkbutton(window, text="Caractères spéciaux", variable=special_var)
-    special_check.grid(row=1, column=2, padx=5, pady=5)
+    if not password or not name:
+        messagebox.showerror("Erreur", "Veuillez saisir un nom et un mot de passe.")
+        return
 
-    # Ajouter un widget pour afficher le mot de passe généré
-    password_frame = ttk.Frame(window)
-    password_frame.grid(row=2, column=0, columnspan=3, padx=5, pady=5)
-    password_label = tk.Label(password_frame, text="")
-    password_label.pack(side=tk.LEFT)
-    copy_button = ttk.Button(password_frame, text="Copier", command=lambda: pyperclip.copy(password_label.cget("text")) if password_label.cget("text") else None, state=tk.DISABLED)
-    copy_button.pack(side=tk.LEFT)
-    export_button = ttk.Button(password_frame, text="Exporter sous TXT", command=lambda: export_password(password_label.cget("text")) if password_label.cget("text") else None, state=tk.DISABLED)
-    export_button.pack(side=tk.LEFT)
+    # Crypte le mot de passe
+    key = Fernet.generate_key()
+    cipher_suite = Fernet(key)
+    encrypted_password = cipher_suite.encrypt(password.encode())
 
-    # Ajouter un bouton pour générer le mot de passe
-    def generate():
-        length = int(length_entry.get())
-        include_digits = digits_var.get()
-        include_uppercase = uppercase_var.get()
-        include_special = special_var.get()
-        password = generate_password(length, include_digits, include_uppercase, include_special)
-        password_label.config(text=password)
-        copy_button.config(state=tk.NORMAL if password else tk.DISABLED)
-        export_button.config(state=tk.NORMAL if password else tk.DISABLED)
+    with open("passwords.txt", "a") as file:
+        file.write(f"{name}: {encrypted_password.decode()}\n")
 
-    generate_button = tk.Button(window, text="Générer", command=generate, state=tk.DISABLED)
-    generate_button.grid(row=3, column=0, columnspan=3, padx=5, pady=5)
+    messagebox.showinfo("Succès", "Le mot de passe a été enregistré avec succès.")
 
+# Crée une fenêtre
+window = tk.Tk()
+window.title("Générateur de mot de passe")
+window.geometry("400x250")
 
-    # Vérifier que les options de mot de passe sont correctement définies
-    def check_options():
-        length = length_entry.get()
-        digits = digits_var.get()
-        uppercase = uppercase_var.get()
-        special = special_var.get()
-        if length.isdigit() and int(length) > 0 and (digits or uppercase or special):
-            generate_button.config(state=tk.NORMAL)
-        else:
-            generate_button.config(state=tk.DISABLED)
+# Étiquette et champ pour le nom du mot de passe
+name_label = tk.Label(window, text="Nom:")
+name_label.pack()
+name_entry = tk.Entry(window)
+name_entry.pack()
 
-    length_entry.bind("<KeyRelease>", lambda event: check_options())
-    digits_var.trace_add("write", lambda *args: check_options())
-    uppercase_var.trace_add("write", lambda *args: check_options())
-    special_var.trace_add("write", lambda *args: check_options())
+# Longueur du mot de passe
+length_label = tk.Label(window, text="Longueur:")
+length_label.pack()
+length_entry = tk.Entry(window)
+length_entry.insert(tk.END, "8")  # Longueur par défaut
+length_entry.pack()
 
-# Afficher la fenêtre Tkinter
-    window.mainloop()
+# Options de composition du mot de passe
+options_frame = tk.LabelFrame(window, text="Options")
+options_frame.pack(fill=tk.BOTH, expand=True)
 
+lowercase_var = tk.BooleanVar()
+lowercase_check = tk.Checkbutton(options_frame, text="Minuscules", variable=lowercase_var)
+lowercase_check.pack(anchor=tk.W)
 
-generate_password_gui()
+uppercase_var = tk.BooleanVar()
+uppercase_check = tk.Checkbutton(options_frame, text="Majuscules", variable=uppercase_var)
+uppercase_check.pack(anchor=tk.W)
+
+digits_var = tk.BooleanVar()
+digits_check = tk.Checkbutton(options_frame, text="Chiffres", variable=digits_var)
+digits_check.pack(anchor=tk.W)
+
+special_chars_var = tk.BooleanVar()
+special_chars_check = tk.Checkbutton(options_frame, text="Caractères spéciaux", variable=special_chars_var)
+special_chars_check.pack(anchor=tk.W)
+
+# Étiquette et champ pour le mot de passe généré
+password_label = tk.Label(window, text="Mot de passe:")
+password_label.pack()
+password_entry = tk.Entry(window)
+password_entry.pack()
+
+# Bouton pour générer un mot de passe
+generate_button = tk.Button(window, text="Générer", command=generate_password)
+generate_button.pack()
+
+# Bouton pour enregistrer le mot de passe
+save_button = tk.Button(window, text="Enregistrer", command=save_password)
+save_button.pack()
+
+# Lance la boucle principale de l'interface graphique
+window.mainloop()
